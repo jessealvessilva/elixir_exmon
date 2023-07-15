2324cb71e2772fb9411f45041a06be4dd976b41b
Jessé Augusto <jessealvessilva@gmail.com>
2023-07-15 20:24:27 -0300

    Função que cria jogador / uso de alias

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,18 +1,7 @@
 defmodule ExMon do
-  @moduledoc """
-  Documentation for `ExMon`.
-  """
+  alias ExMon.Player
+  def create_player(name, move_rnd, move_avg, move_heal) do
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExMon.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    Player.build( name, move_rnd, move_avg, move_heal )
   end
 end
