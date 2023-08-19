0a3fd3da38fb2ce775aa2c3e856c2879e911f2b9
Jessé Augusto <jessealvessilva@gmail.com>
2023-08-18 23:41:36 -0300

    Atualizações de módulo

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -2,6 +2,7 @@ defmodule ExMon do
   # alias ExMon.Player, as: Banana
   alias ExMon.{Game, Player}
   alias ExMon.Game.{Actions, Status }
+  alias ExMon.Game.Actions
 
   @computer_name "Robotinik"
   def create_player(name, move_rnd, move_avg, move_heal) do
@@ -18,6 +19,17 @@ defmodule ExMon do
   end
 
   def make_move( move ) do
-    Actions.fetch_move( move )
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+  end
+
+  defp do_move( {:error, move }), do: Status.prin_wrong_message( move )
+
+  defp do_move( {:ok   , move }) do
+    case move do
+      :move_heal -> "realiza a cura"
+      move -> Actions.attack(move)
+    end
   end
 end
