6c91476a97992da7e9d3092a5765db56db264a70
Jessé Augusto <jessealvessilva@gmail.com>
2023-09-07 23:28:12 -0300

    Criação da cura

--- lib/actions.ex
+++ lib/actions.ex
@@ -1,6 +1,7 @@
 defmodule ExMon.Game.Actions do
   alias ExMon.Game
-  alias ExMon.Game.Actions.Attack
+  alias ExMon.Game.Actions.{Attack, Heal}
+
 
   def attack(move) do
      case Game.turn() do
@@ -8,6 +9,14 @@ defmodule ExMon.Game.Actions do
         :computer -> Attack.attack_oponent(:player, move)
      end
   end
+
+  def heal() do
+    case Game.turn() do
+       :player   -> Heal.heal_life(:player)
+       :computer -> Heal.heal_life(:computer)
+    end
+  end
+
   def fetch_move( move ) do
     Game.player()
     |> Map.get(  :moves )
